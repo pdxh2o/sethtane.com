c83f465173517c5793f900c67b94daedfb9f414f
Seth Tane <seth@sethtane.com>
2013-12-19 16:47:09 -0800

    hackensack

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,16 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/HackensackT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Over the Hackensack</span><br/>
+			<span class="workDate">2013 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</div>
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/Hess EstateT.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">Hess Estate</span><br/>
