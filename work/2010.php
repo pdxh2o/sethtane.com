ab7f850672b83ed09c624c52f7f7e9febc47f98e
Seth Tane <seth@sethtane.com>
2014-01-11 20:26:45 -0800

    fix bryant4

--- work/2010.php
+++ work/2010.php
@@ -5,9 +5,9 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
-		<img class="thumbnail" src="/paintings/5th / BryantT.jpg" />
+		<img class="thumbnail" src="/paintings/34th Street CT.jpg" />
 		<div class="workDescription">
-			<span class="workTitle">5th / Bryant</span><br/>
+			<span class="workTitle">34th Street C</span><br/>
 			<span class="workDate">2014 </span><br/>
 			<span class="workSize">6-3/4" x 10"</span><br/>
 			<span class="workMedium">Oil on panel</span><br>
@@ -16,9 +16,9 @@ include('work_header.html');
 	</div>
 	
 	<div class="workItem">
-		<img class="thumbnail" src="/paintings/34th Street CT.jpg" />
+		<img class="thumbnail" src="/paintings/5th / BryantT.jpg" />
 		<div class="workDescription">
-			<span class="workTitle">34th Street C</span><br/>
+			<span class="workTitle">5th / Bryant</span><br/>
 			<span class="workDate">2014 </span><br/>
 			<span class="workSize">6-3/4" x 10"</span><br/>
 			<span class="workMedium">Oil on panel</span><br>
