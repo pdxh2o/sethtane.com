b48370e787337f3daff65b9506e0a365591aa1e8
Seth Tane <seth@sethtane.com>
2014-01-11 20:59:12 -0800

    fix bryant12

--- work/2010.php
+++ work/2010.php
@@ -4,15 +4,7 @@ include('work_header.html');
 
 <div class="decadeTitle leftAligned">2010's</div>
 	
-	<div class="workItem">
-		<img class="thumbnail" src="/paintings/5th BryantT.jpg" />
-		<div class="workDescription">
-			<span class="workTitle">5th / Bryant</span><br/>
-			<span class="workDate">2014 </span><br/>
-			<span class="workSize">6-3/4" x 10"</span><br/>
-			<span class="workMedium">Oil on panel</span>
-		</div>
-	</div>
+
 	
 	<div class="workItem">
 		<img class="thumbnail" src="/paintings/34th Street CT.jpg" />
