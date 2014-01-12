36b4822b51d59078edf0611a2a1761b19039d260
Seth Tane <seth@sethtane.com>
2014-01-11 19:50:20 -0800

    34th st. C & 5th / Bryant added

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,28 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/5th / BryantT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">5th / Bryant</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span><br>
+			<?php if (isset($_COOKIE['vip'])) { ?><span class="workPrice">$1,500</span><?php } ?>
+		</div>
+	</div>
+	
+	<div class="workItem">
+		<img class="thumbnail" src="/paintings/34th Street CT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">34th Street C</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span><br>
+			<?php if (isset($_COOKIE['vip'])) { ?><span class="workPrice">$1,500</span><?php } ?>
+		</div>
+	</div>
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/HackensackT.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">Over the Hackensack</span><br/>
