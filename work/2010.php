2a2f2feeb60a6ccd4f54c8040c8d49faecc5f916
Seth Tane <seth@sethtane.com>
2014-01-21 15:59:57 -0800

    add lonely planet

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,16 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/Lonely PlatformT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Lonely Platform</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</div>
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/A trainT.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">A Train</span><br/>
