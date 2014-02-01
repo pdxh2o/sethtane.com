1933da958365f12b2ed5e9d7ddce41e02081f473
Seth Tane <seth@sethtane.com>
2014-02-01 10:53:16 -0800

    add empire state and newark

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,26 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/NewarkT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Newark</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</di
+	
+	<div class="workItem">
+		<img class="thumbnail" src="/paintings/Empire StateT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Empire State</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</di
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/7th AvT.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">7th Av</span><br/>
