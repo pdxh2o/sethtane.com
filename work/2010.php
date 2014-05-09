e9d6b985672ca1c3d4e5daa4c35a56a2d61a70f1
Seth Tane <seth@sethtane.com>
2014-05-09 07:31:24 -0700

    add deserted

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,17 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/DesertedT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Deserted</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</div>
+	
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/Upper deckT.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">Upper Deck</span><br/>
