6c83a134e1bb05334ea7476dc64a3a1ca3843e17
Seth Tane <seth@sethtane.com>
2014-06-18 20:04:14 -0700

    tank car

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,16 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/Tank CarT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Tank Car</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</div>
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/Coalinga-148T.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">Coalinga 148</span><br/>
