b5840d526ea96f79fb6775ca7dab099831c0f47b
Seth Tane <seth@sethtane.com>
2014-07-16 19:44:10 -0700

    add sammi crystal

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,16 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/Sammi CrystalT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Sammi Crystal</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</div>
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/New AmbitionT.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">New Ambition</span><br/>
