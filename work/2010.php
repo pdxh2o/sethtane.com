2843ca60e9f9df5c28878ce32faea2463bd745a7
Seth Tane <seth@sethtane.com>
2014-08-07 20:41:45 -0700

    add art co

--- work/2010.php
+++ work/2010.php
@@ -5,6 +5,16 @@ include('work_header.html');
 <div class="decadeTitle leftAligned">2010's</div>
 	
 	<div class="workItem">
+		<img class="thumbnail" src="/paintings/Art CoT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">the Art Co.</span><br/>
+			<span class="workDate">2014 </span><br/>
+			<span class="workSize">6-3/4" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</div>
+	
+	<div class="workItem">
 		<img class="thumbnail" src="/paintings/Larkin WombatT.jpg" />
 		<div class="workDescription">
 			<span class="workTitle">the Larkin Wombat</span><br/>
