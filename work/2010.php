02dc2c7eb18c3d11427d40563b7e846625671066
Seth Tane <seth@sethtane.com>
2015-09-22 23:02:25 -0700

    add switcher

--- work/2010.php
+++ work/2010.php
@@ -3,6 +3,15 @@ include('work_header.html');
 ?>
 
 <div class="decadeTitle leftAligned">2010's</div>
+	<div class="workItem">
+		<img class="thumbnail" src="/paintings/SwitcherT.jpg" />
+		<div class="workDescription">
+			<span class="workTitle">Switcher</span><br/>
+			<span class="workDate">2015 </span><br/>
+			<span class="workSize">6.75" x 10"</span><br/>
+			<span class="workMedium">Oil on panel</span>
+		</div>
+	</div>
 	
 	<div class="workItem">
 		<img class="thumbnail" src="/paintings/CrossroadsT.jpg" />
