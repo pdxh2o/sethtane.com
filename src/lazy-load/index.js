0bca551621c45b518ed4ee4871fa242efce3e8b1
Jesse Tane <jesse.tane@gmail.com>
2015-09-29 18:52:02 -0400

    add super sweet image load spinners

--- src/lazy-load/index.js
+++ src/lazy-load/index.js
@@ -7,6 +7,12 @@ function LazyLoader () {
 
 LazyLoader.prototype.scan = function () {
   this.images = Array.prototype.slice.call(document.querySelectorAll('[lazy-src]'))
+  this.images.forEach(function (image) {
+    var lazySrc = image.getAttribute('lazy-src')
+    if (lazySrc) {
+      image.parentNode.classList.add('lazy-loading')
+    }
+  })
   this._onscroll()
 }
 
@@ -16,17 +22,19 @@ LazyLoader.prototype._onscroll = function () {
 }
 
 LazyLoader.prototype._doload = function () {
-  for (var i = 0; i < this.images.length; i++) {
-    var image = this.images[i]
+  this.images.forEach(function (image) {
     var rect = image.getBoundingClientRect()
     if (rect.top > -image.parentNode.offsetHeight && rect.top <= document.body.clientHeight) {
       var lazySrc = image.getAttribute('lazy-src')
       if (lazySrc) {
         image.setAttribute('src', lazySrc)
         image.removeAttribute('lazy-src')
+        image.onload = function () {
+          image.parentNode.classList.remove('lazy-loading')
+        }
       }
     }
-  }
+  })
 }
 
 module.exports = new LazyLoader()
