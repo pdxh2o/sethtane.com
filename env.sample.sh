4570e82509dd7c8ce7a291602db3531bd84bb123
Jesse Tane <jesse.tane@gmail.com>
2015-12-05 02:22:20 -0500

    minify in production

--- env.sample.sh
+++ env.sample.sh
@@ -4,5 +4,5 @@ export CMS_SPACE="h8jskevcughh"
 export CMS_KEY="aa4188f85f37b46343cdee82f2263db2169160b63322efc493b0cd2fd826b9e9"
 export CMS_OAUTH_TOKEN="" # only required for admin scripts
 export CDN_URL="https://s3.amazonaws.com/sethtane.com"
-export RESIZE_URL="http://sethtane-com.imgix.net/"
+export RESIZE_URL="http://sethtane-com.imgix.net"
 export IMAGE_QUALITY="50"
