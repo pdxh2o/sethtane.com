9902598387c3256bfc0ee1cd58cf482f47bf6223
Jesse Tane <jesse.tane@gmail.com>
2020-08-31 14:11:24 -0400

    works: remove svg width and height

--- src/works/index.js
+++ src/works/index.js
@@ -17,14 +17,14 @@ var sizes = {
 }
 
 var audioPlayImage = `<?xml version="1.0"?>
-<svg xmlns="http://www.w3.org/2000/svg" version="1.0"  width="500" height="500" viewBox="0 0 75 75">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 75 75">
 <path d="M39.389,13.769 L22.235,28.606 L6,28.606 L6,47.699 L21.989,47.699 L39.389,62.75 L39.389,13.769z"
 style="stroke:#111;stroke-width:5;stroke-linejoin:round;fill:#111;"
 />
 <path d="M48,27.6a19.5,19.5 0 0 1 0,21.4M55.1,20.5a30,30 0 0 1 0,35.6M61.6,14a38.8,38.8 0 0 1 0,48.6" style="fill:none;stroke:#111;stroke-width:5;stroke-linecap:round"/>`
 
 var audioStopImage = `<?xml version="1.0"?>
-<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="500" height="500" viewBox="0 0 75 75"
+<svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 75 75"
 stroke="#111" stroke-width="5">
 <path d="m39,14-17,15H6V48H22l17,15z" fill="#111" stroke-linejoin="round"/>
 <path d="m49,26 20,24m0-24-20,24" fill="none" stroke-linecap="round"/>`
